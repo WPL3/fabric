aeafcbbe51918a47dd200b1aa2cb55996dd61085
WPL3 <zubaerimran04@gmail.com>
2025-10-17 00:17:02 +0600

    Very important thing

---
+++ src/main/java/com/wpl3/training/FirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wpl3.training;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/wpl3/training/Trainingclient.java
@@ -0,0 +1,10 @@
+package com.wpl3.training;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Trainingclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
